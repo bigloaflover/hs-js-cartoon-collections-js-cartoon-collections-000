555276b8bdec0bb52b5de3d149e6f47ac79889cd
droll-demo-7630 <danjamrhi@hotmail.com>
2020-10-08 19:16:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
 function dwarfRollCall(dwarves) {
+  let listDwarves = []
+  for(let i = 0; i < dwarves.length; i++){
+    
+    listDwarves.push(`${i+1}. ${dwarves[i]} `)
+    
+  }
+  return listDwarves.join(""); 
 }
+
 
 function summonCaptainPlanet(planeteerCalls){
+  let uppercasePlaneteerCalls = [];
+  for(let i = 0; i < planeteerCalls.length; i++){
+    let punctuation = "!"
+    uppercasePlaneteerCalls.push(`${planeteerCalls[i]}${punctuation}`.toUpperCase())
+  }
+  return uppercasePlaneteerCalls
 }
+
+
 
 function longPlaneteerCalls(words) {
+  
+  for(let i = 0; i < words.length; i++){
+    
+    if(words[i].length>4){
+      return true
+    }
+  }
+  return false
 }
 
+
+
 function findTheCheese (foods) {
+  
+  for (var i = 0; i < foods.length; i++) {
+    
+    if (foods[i] ===  'gouda'|| foods[i]===  'cheddar' || foods[i]==='camembert') {
+      return foods[i];
+    }
+  }
+  return 'no cheese!'
 }
